72fddfebc0b2fd3f941404f4eaa4973eb0e68947
Illia Zybin <izybin97@gmail.com>
2019-09-17 21:56:56 +0300

    first commit

---
+++ src/main/java/com/illias/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.illias.springmvc.dao;
+
+import java.util.List;
+
+import com.illias.springmvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
